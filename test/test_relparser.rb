9f26a0288c811867514d7f3c7e22ae53cd3a0149
Fredrik Leijon <fredrik.leijon@gmail.com>
2014-01-28 20:38:11 +0100

    test entry point

--- test/test_relparser.rb
+++ test/test_relparser.rb
@@ -4,8 +4,8 @@ require 'relparser'
 class RelParserTests < Test::Unit::TestCase
 
   def test_parse_returns_release
-		#result = RelParser.parse "foo"
-		#assert_instance_of RelParser::Release, result
+		result = RelParser.parse "Homeland.S03E12.PROPER.720p.HDTV.x264-KILLERS"
+		assert_equal("Homeland",result.name)
 	end
 
 end
